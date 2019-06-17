8bba73a8f73b549b9d692de3ea9221a2dd7eb18c
KaiyuanLiu <kaiyuan@KaiyuanLius-MacBook-Pro.local>
2019-06-17 15:09:00 +0800

    add plane class

---
+++ class/plane.h
@@ -0,0 +1,31 @@
+//
+// Created by KaiyuanLiu on 2019-06-17.
+//
+
+#ifndef GRAPHICS_PLANE_H
+#define GRAPHICS_PLANE_H
+#include "../aux/shader.h"
+#include "../aux/stb_image.h"
+const float planeVertices[] = {
+        // positions            // normals         // texcoords
+        25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+
+        25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+        25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
+};
+
+class Plane {
+public:
+    Plane();
+    void Draw(Shader& shader);
+private:
+    unsigned int VAO;
+    unsigned int Texture;
+    unsigned int LoadTexture(char const* path);
+};
+
+
+#endif //GRAPHICS_PLANE_H
